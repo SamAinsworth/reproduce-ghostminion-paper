2fc1d3ec0d0c576d9214a2ec81f10c6b6a4e5172
Sam Ainsworth <sam.ainsworth@cl.cam.ac.uk>
2021-02-10 14:52:55 +0000

    2017

--- scripts/build_spec17.sh
+++ scripts/build_spec17.sh
@@ -9,7 +9,7 @@ cd ..
 sudo umount specmnt
 rm -r specmnt
 cp spec_confs/aarch64_17.cfg SPEC17/config
-cd SPEC
+cd SPEC17
 . ./shrc   
 runcpu --config=aarch64_17.cfg --action=build perlbench gcc mcf omnetpp xalancbmk x264 deepsjeng leela exchange2 xz bwaves cactusBSSN lbm wrf cam4 pop2 imagick nab fotonik3d roms -I
 runcpu --config=aarch64_17.cfg --action=run --size=ref perlbench gcc mcf omnetpp xalancbmk x264 deepsjeng leela exchange2 xz bwaves cactusBSSN lbm wrf cam4 pop2 imagick nab fotonik3d roms --noreportable --iterations=1  -I
